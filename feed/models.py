bffde4a9ef988fface1937f8da6266c36e8e823d
Robert Vincent <redfordrobert68@gmail.com>
2023-07-23 23:36:52 -0400

    Continued development of social media app

--- feed/models.py
+++ feed/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     text = models.CharField(max_length=240)
     date = models.DateTimeField(auto_now = True) #auto_now makes field uneditable (won't appear in database)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self): # Every method in on a Python class takes "self" as its first parameter
         return self.text[0:100] #Limit post title to 100 characters
--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from .models import Post
 
 class HomePage(ListView): # Render out a template with all of the posts
@@ -7,3 +10,41 @@ class HomePage(ListView): # Render out a template with all of the posts
     model = Post
     context_object_name = "posts" # By default it's called "objects"
     queryset = Post.objects.all().order_by('-id')[0:30] # 30 max posts loaded per page
+
+class PostDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = "feed/detail.html"
+    model = Post
+    context_object_name = "post" # Change name to match model
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "feed/create.html" # View more about create view at ccbv.co.uk
+    fields = ["text"]
+    success_url = "/"
+
+    def post(self, request, *args, **kwargs):
+        post = Post.objects.create(
+            text=request.POST.get("text"),
+            author=request.user,
+        )
+
+        return render(
+            request, # Always takes request as it's first render
+            "includes/post.html",
+            {
+                "post":post,
+                "show_detail_link":True, 
+            },
+            content_type="application/html"
+        )
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user # Needs to be thrown into dispatch
+        obj.save()
+        return super().form_valid(form)
